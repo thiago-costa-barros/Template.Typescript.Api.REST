257f3beaa3745905e7b757dd2987bcb17b86f5ed
Thiago Costa Barros <thiagoc.barros92@gmail.com>
2025-03-22 15:12:57 -0300

    feat: add express config inicial

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("teste oi");
+import {config} from 'dotenv'
+import express from 'express'
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
