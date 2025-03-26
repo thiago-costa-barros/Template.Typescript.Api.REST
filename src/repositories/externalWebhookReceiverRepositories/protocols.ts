77c7f3264d67e26cba70d85bf31d16643677791a
Thiago Costa Barros <thiagoc.barros92@gmail.com>
2025-03-26 10:39:25 -0300

    delete: remoção de arquivos de interface

--- src/repositories/externalWebhookReceiverRepositories/protocols.ts
+++
@@ -1,8 +0,0 @@
-import { ExternalWebhookReceiver } from "@prisma/client";
-import { ExternalWebhookReceiverHotmartDTO } from "src/controllers/externalWebhookReceiverControllers/ExternalWebhookReceiverControllerDTO";
-
-export interface ICreateExternalWebhookReceiverHotmartRepository {
-  createExternalReceiverWebhookHotmart(
-    externalReceiverWebhookHotmartData: ExternalWebhookReceiverHotmartDTO
-  ): Promise<ExternalWebhookReceiver>;
-}
