4fa889f7ce1be302358a3d523636513bee82cc4e
Thiago Costa Barros <thiagoc.barros92@gmail.com>
2025-03-29 08:47:17 -0300

    feat: add tratamento de erros no server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import { config } from "dotenv";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import routes from "./routes/Routes";
 import "tsconfig-paths/register";
+import { errorHandler } from "./middlewares/ErrorHandlerMiddleware";
 
 config();
 
@@ -11,6 +12,10 @@ app.use(express.json());
 const port = process.env.PORT;
 
 app.use("/api", routes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  errorHandler(err, req, res, next);
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
